28cce8d4b76f178f29ad146f95d6b04c6663d797
Marcel Clausen <marcel.clausen@swisstopo.ch>
2022-09-19 18:15:11 +0200

    switch to sphinx api call

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,11 @@ from flask import make_response
 from flask import request
 
 from app import app
+from app.lib import sphinxapi
 from app.search import Search
+from app.settings import SEARCH_SPHINX_HOST
+from app.settings import SEARCH_SPHINX_PORT
+from app.settings import SEARCH_SPHINX_TIMEOUT
 from app.version import APP_VERSION
 
 logger = logging.getLogger(__name__)
@@ -18,19 +22,25 @@ def checker():
 
 @app.route('/checker/ready', methods=['GET'])
 def readiness():
-    with app.test_client() as testclient:
-        resp = testclient.get(
-            '/rest/services/all/SearchServer',
-            query_string={
-                'type': 'locations', 'searchText': 'Landstrasse 78'
-            }
-        )
+    # set up sphinx client
+    sphinx = sphinxapi.SphinxClient()
+    sphinx.SetServer(SEARCH_SPHINX_HOST, SEARCH_SPHINX_PORT)
+    sphinx.SetConnectTimeout(SEARCH_SPHINX_TIMEOUT)
+    sphinx.SetMatchMode(sphinxapi.SPH_MATCH_EXTENDED)
 
-    if resp.status_code == 200:
+    # run query
+    result = sphinx.Query('nofx', 'swisssearch')
+    sphinx_status = {
+        'data': result if result is not None else 'ERROR or WARNING',
+        'error_msg': sphinx.GetLastError(),
+        'warning_msg': sphinx.GetLastWarning()
+    }
+
+    if result:
         return make_response(jsonify({'success': True, 'message': 'OK'}))
 
-    logger.critical('failed to access sphinxsearch error: %s', resp.data)
-    return None
+    logger.critical(sphinx_status)
+    return make_response(jsonify({'success': False, 'status': sphinx_status}), 503)
 
 
 @app.route('/rest/services/<topic>/SearchServer', methods=['GET'])
